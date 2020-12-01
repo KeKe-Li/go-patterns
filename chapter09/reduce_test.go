a14b5431cd29fec631af4331db3bc438400f9b13
lisensen <lisensen@innotechx.com>
2020-12-01 16:59:45 +0800

    feat: change page of reduce

---
+++ chapter09/reduce_test.go
@@ -0,0 +1,26 @@
+package chapter09
+
+import (
+	"testing"
+)
+
+func mul(a, b int) int {
+	return a * b
+}
+
+func TestReduce(t *testing.T) {
+	a := make([]int, 10)
+	for i := range a {
+		a[i] = i + 1
+	}
+	// Computer 10
+	out := Reduce(a, mul, 1)
+	expect := 1
+	for i := range a {
+		expect *= a[i]
+	}
+	if expect != out {
+		t.Fatalf("expected %d got %d", expect, out)
+	}
+
+}
