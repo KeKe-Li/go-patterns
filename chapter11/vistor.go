cf62351ef32b216e4aa8ccc81bd4d26525aea22f
lisensen <lisensen@innotechx.com>
2020-12-02 14:42:54 +0800

    feat: add visitor

---
+++ chapter11/vistor.go
@@ -0,0 +1,68 @@
+package chapter11
+
+import (
+	"fmt"
+	"math"
+)
+
+type Info struct {
+	Namespace   string
+	Name        string
+	OtherThings string
+}
+
+type VisitorFunc func(*Info, error) error
+
+type Visitor interface {
+	Visitor(VisitorFunc) error
+}
+
+func (info *Info) Visit(fn VisitorFunc) error {
+	return fn(info, nil)
+}
+
+type OtherThingsVisitor struct {
+	visitor Visitor
+}
+
+func (o OtherThingsVisitor) Visit(fn VisitorFunc) error {
+	return o.visitor.Visitor(func(info *Info, err error) error {
+		fmt.Println("OtherThingsVisitor() before call function")
+		err = fn(info, err)
+		if err == nil {
+			fmt.Printf("===> OtherThings=%s\n", info.OtherThings)
+		}
+		fmt.Println("OtherThingsVisitor() after call function ")
+		return err
+	})
+}
+
+type LogVisitor struct {
+	visitor Visitor
+}
+
+func (o LogVisitor) Visit(fn VisitorFunc) error {
+	return o.visitor.Visitor(func(info *Info, err error) error {
+		fmt.Println("LogVisitor() before call function")
+		err = fn(info, err)
+		fmt.Println("LogVisitor() after call function")
+		return err
+	})
+}
+
+type NameVisitor struct {
+	vistor Visitor
+}
+
+func (v NameVisitor) Visit(fn VisitorFunc) error {
+	return v.vistor.Visitor(func(info *Info, err error) error {
+		fmt.Println("NameVisitor() before call function")
+		err = fn(info, err)
+		if err == nil {
+			fmt.Printf("===> Name=%s,NameSpace=%s\n", info.Name, info.Namespace)
+		}
+		fmt.Println("NameVisitor() after call function")
+		return err
+	})
+}
+
